c4524be93c5fdd35d055c7f623d4a2202f8d982d
Naveen-kumar01 <80191032+Naveen-kumar01@users.noreply.github.com>
2021-11-24 15:04:18 +0530

    updated cement-strength-code

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cement-strength-heroku",
+    name="cement-strength-prediction",
     version="0.0.3",
     description="ML project",
     author="Naveen Kumar",
